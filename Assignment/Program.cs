b7d8bd0906e8b3851a8e71b3a5896925312dea3c
therealitees1 <itohowonkanang@gmail.com>
2026-01-07 14:18:16 +0100

    Assignment

---
+++ Assignment/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("My name is Itohowo Nkanang");
+Console.WriteLine("I am learning C# programming.");
+Console.WriteLine("This is my first C# application.");
+Console.WriteLine("I enjoy coding and building applications.");
+Console.WriteLine("C# is a powerful language for developing software.");
+Console.WriteLine("I look forward to mastering C# and creating amazing projects.");
+Console.WriteLine("Thank you for reading my first C# program!");
--- FirstApp/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("My name is Itohowo Nkanang");
-Console.WriteLine("I am learning C# programming.");
-Console.WriteLine("This is my first C# application.");
-Console.WriteLine("I hope to build many more applications in the future.");
