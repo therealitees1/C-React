fa181f64fc7068941a6f5244e4170ff5f32681c8
therealitees1 <itohowonkanang@gmail.com>
2026-01-30 21:30:00 +0100

    RandomOtp

---
+++ RandomOtp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Length of the OTP (change as desired)
+        int otpLength = 6;
+
+        // Generate OTP
+        string otp = GenerateNumericOtp(otpLength);
+        Console.WriteLine($"Generated OTP: {otp}");
+
+        // Example: prompt user to enter OTP to validate
+        Console.Write("Enter the OTP: ");
+        string input = Console.ReadLine();
+
+        if (input == otp)
+            Console.WriteLine("OTP is correct!");
+        else
+            Console.WriteLine("OTP is incorrect.");
+    }
+         // Generates a numeric OTP of the requested length
+    static string GenerateNumericOtp(int length)
+    {
+        if (length <= 0) return "";
+
+        var rng = new Random();
+        char[] digits = new char[length];
+
+        for (int i = 0; i < length; i++)
+        {
+            digits[i] = (char)('0' + rng.Next(0, 10)); // random digit 0-9
+        }
+             return new string(digits);
+        }
+    }
