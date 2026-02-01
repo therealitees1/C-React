b1a69ec16f2e467e74c003af8055c77058ec0a7c
therealitees1 <itohowonkanang@gmail.com>
2026-02-01 13:13:41 +0100

    ReverseArray

---
+++ ReverseArray/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = { 1, 2, 3, 4, 5 };
+        int[] reversed = new int[array.Length];
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            reversed[i] = array[array.Length - 1 - i];
+        }
+
+        foreach (int num in reversed)
+        {
+            Console.Write(num + " ");
+        }
+    }
+}
