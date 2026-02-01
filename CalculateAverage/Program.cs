619090895e595f0c9a94e7beb0e7fc15d10cfe87
therealitees1 <itohowonkanang@gmail.com>
2026-02-01 13:12:05 +0100

    CalculateAverage

---
+++ CalculateAverage/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("How many numbers? ");
+        int count = int.Parse(Console.ReadLine());
+
+        double sum = 0;
+
+        for (int i = 1; i <= count; i++)
+        {
+            Console.Write("Enter number " + i + ": ");
+            sum += double.Parse(Console.ReadLine());
+        }
+
+        double average = sum / count;
+        Console.WriteLine("Average = " + average);
+    }
+}
